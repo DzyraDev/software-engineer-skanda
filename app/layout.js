abb7aa2d8e55162b803b45c54c3666b0af62208e
dzyradev <116470817+DzyraDev@users.noreply.github.com>
2024-06-08 23:33:05 +0700

    feat: speed insight

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,13 @@
-import { GoogleTagManager } from '@next/third-parties/google';
-import { Inter } from 'next/font/google';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Footer from './components/footer';
-import Navbar from './components/navbar';
-import './css/card.scss';
-import './css/globals.scss';
-const inter = Inter({ subsets: ['latin'] });
+import { GoogleTagManager } from "@next/third-parties/google";
+import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+import "./css/card.scss";
+import "./css/globals.scss";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: " SOFTWARE ENGINEERING - SMKN 2 BDL",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
         <Footer />
       </body>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+      <SpeedInsights />
     </html>
-  )
-};
+  );
+}
