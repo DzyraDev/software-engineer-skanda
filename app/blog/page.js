4d3fab4cca392f036595b42201121eb5940c073e
dzyradev <116470817+DzyraDev@users.noreply.github.com>
2024-06-08 23:40:41 +0700

    feat: improve

--- app/blog/page.js
+++ app/blog/page.js
@@ -5,12 +5,12 @@ import BlogCard from "../components/homepage/blog/blog-card";
 
 function Page() {
   return (
-    <div className="py-8">
-      <div className="flex justify-center my-5 lg:py-8">
+    <div className="py-10">
+      <div className="flex justify-center text-center my-5 lg:py-8">
         <div className="flex  items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
-            All Blog
+            All Portfolio
           </span>
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
         </div>
